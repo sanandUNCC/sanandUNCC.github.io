71984cee1ca0018479c369eb37bd841690722997
SaiTheBrownGuy <sanand8@uncc.edu>
2021-10-03 22:10:00 -0400

    major
    made simple calc, attempted code and now set up calc fcc js and html
    minor h2 in main issues

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,25 @@
+function showNum(num){
+    //alert(num);
+    return num;
+}
+
+function add(a, b){
+    alert("a + b = " + (a + b))
+}
+
+function divide(a, b){
+    alert("a / b = " + (a / b))
+}
+
+function multiply(a, b){
+    alert("a * b = " + (a * b))
+}
+
+function subtract(a,b){
+    alert("a - b = " + (a - b))
+}
+
+var a = showNum(num);
+var b = showNum(num);
+
+
