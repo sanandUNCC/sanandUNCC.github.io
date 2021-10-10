d8b45b95d7463b8ab6d7f5fafe046bb90c9e098a
SaiTheBrownGuy <sanand8@uncc.edu>
2021-10-09 22:38:10 -0400

    Major
    Made arrays html

---
+++ itis3135/scripts/arrays.js
@@ -0,0 +1,22 @@
+var person = [];
+var salaries = [];
+
+function displayResults(){
+
+}
+
+function displaySalary(){
+
+}
+
+function addSalary(){
+    console.log("pre");
+
+    var salIn = document.getElementById().value;
+    var nameIn = document.getElementById().value;
+
+    person.push(nameIn);
+    salaries.push(salIn);
+
+    console.log("post");
+}
