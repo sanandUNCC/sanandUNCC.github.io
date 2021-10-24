d2d6cc77ebad9d59c69e7a014af0d4ce8435aecd
SaiTheBrownGuy <sanand8@uncc.edu>
2021-10-23 23:48:09 -0400

    Major
    additions to slideshow

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,13 +1,28 @@
-$(document).ready(function() {
-	$(".slide:first").show();
-	setInterval(function(){ Next($('.slide:visible'))}, 2400);
+$(document).ready(() => {
+    $("#slide_images a").each(function() {
+        var slide = new Image();
+        slide.src = $(this).attr("href");
+
+    });
+
+    $("#slide_images a").click(function(evt) { 
+        var imageURL = $(this).attr("href"); 
+        $("#main_image").attr("src", imageURL); 
+        evt.preventDefault();
+    });
+
+    $(".slide:first").show();
+    setInterval(function () { Next($('.slide:visible')) }, 2400);
+
+
 
 });
+
 function Next(slide) {
-	slide.fadeOut();
-	if(typeof slide.next().attr('src') !== 'undefined') {
-		slide.next().fadeIn();
-	} else {
-		$('.slide:first').fadeIn();
-	}
+    slide.fadeOut();
+    if (typeof slide.next().attr('src') !== 'undefined') {
+        slide.next().fadeIn();
+    } else {
+        $('.slide:first').fadeIn();
+    }
 }
