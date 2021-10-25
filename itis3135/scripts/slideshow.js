c918e3e3b82a5fc42334e1b275df584acb6a9829
SaiTheBrownGuy <sanand8@uncc.edu>
2021-10-24 22:56:45 -0400

    Major
    slideshow html finished and other assignments updated

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,28 +1,23 @@
-$(document).ready(() => {
+//code is the framework provided by the book without the caption segment
+$(document).ready(function() {
+    //code segment preloads the images from the list in the HTML
     $("#slide_images a").each(function() {
+        //there is a new image made each loop
         var slide = new Image();
+        //attaches the image's src to the href attribute of the image being looked
         slide.src = $(this).attr("href");
-
     });
 
-    $("#slide_images a").click(function(evt) { 
-        var imageURL = $(this).attr("href"); 
-        $("#main_image").attr("src", imageURL); 
+    //code segment displays the image clicked on
+    $("#slide_images a").click(function(evt) {
+        //slideURL stores the href attribute of the image being looked at 
+        var slideURL = $(this).attr("href");  
+        //the element with the class "slide_main_image" has its src attribute 
+        //set to the slideURL variable
+        $(".slide_main_image").attr("src", slideURL); 
+        //the textbook says that this cancel's the default action of the link
         evt.preventDefault();
     });
 
-    $(".slide:first").show();
-    setInterval(function () { Next($('.slide:visible')) }, 2400);
-
-
-
 });
 
-function Next(slide) {
-    slide.fadeOut();
-    if (typeof slide.next().attr('src') !== 'undefined') {
-        slide.next().fadeIn();
-    } else {
-        $('.slide:first').fadeIn();
-    }
-}
