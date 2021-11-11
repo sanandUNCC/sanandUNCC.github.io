9d875d296fd1c4fd298638c159839fbff12284cc
SaiTheBrownGuy <sanand8@uncc.edu>
2021-11-11 16:06:39 -0500

    Major
    finished activity 12

---
+++ itis3135/excercises/Updated-Activity12/Anand-Activity12.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        randomStart: true,
+        auto: true,
+        autoPlay: 3,
+        speed: 1000,
+        captions: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20
+    });
+});
