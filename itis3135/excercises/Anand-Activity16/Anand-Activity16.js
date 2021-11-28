f2b33226c57834e1dbb78409400050499074aa81
SaiTheBrownGuy <sanand8@uncc.edu>
2021-11-28 02:40:20 -0500

    Major
    finished activity 15 and 16

---
+++ itis3135/excercises/Anand-Activity16/Anand-Activity16.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    let url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+
+        $.getJSON(url, function(data) {
+            var html = "";
+            $.each(data.items, function(i, item) {
+                html += "<a href=" + item.media.m + "data-lightbox='flickR'" + "data-title=" + item.title + ">"
+                + "<img src=" + item.media.m + ">" 
+                + "</a>"; 
+            });
+            $("#new_building").html(html);
+
+        });
+
+});
